3c8492a09ac0587ee5a9b6d0b1a502dae8ce35ec
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-23 20:18:49 +0100

    Add: Implement 2-print_alphabet.c to print letters from a to z

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - affiche la lettre a jusqu'à la lettre z
+ *
+ * Return:0 Si rien ne s'affiche tout est parfait
+ */
+int main(void)
+{
+	char lettre;
+
+	for (lettre = 'a' ; lettre <= 'z'; lettre++)
+	{
+		putchar(lettre);
+	}
+	return (0);
+}
