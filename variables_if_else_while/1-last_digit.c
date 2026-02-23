61dc0ae5e16fe73ff3d1c29605f0d3c16e2c910a
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-23 19:34:00 +0100

    Add: Implement 1-last_digit.c to print the last digit of a random number

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/** more headers goes there */
+
+/**
+ * main - print le dernier le chiffre du nombre donné par le programme
+ *
+ * Return:0 Si rien ne s'affiche tout est parfait
+ */
+int main(void)
+{
+	int n;
+	int lastdigi;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdigi = n % 10;
+
+	if (lastdigi > 5)
+	{
+		printf("Last digit of %d is %d and is greather than 5\n", n, lastdigi);
+	}
+	else if (lastdigi == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastdigi);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigi);
+	}
+
+	return (0);
+}
