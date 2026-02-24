5eb255ed5e0be8d20d59310ef099397fc84b2b68
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-24 14:27:29 +0100

    Add: Implement print_alphabet_x10 function and update header file

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - affiche l'alphabet 10x
+ *
+ * Return:0 Si rien ne s'affiche tout est parfait
+ */
+void print_alphabet_x10(void)
+{
+char lettre;
+int compteur = 0;
+
+while (compteur < 10)
+{
+for (lettre = 'a'; lettre <= 'z'; lettre++)
+{
+_putchar(lettre);
+}
+compteur++;
+}
+}
