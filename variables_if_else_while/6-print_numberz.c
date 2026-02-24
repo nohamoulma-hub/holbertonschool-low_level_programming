dc6d9cd8d1a6adea613f4b15fdfcdab3527be73a
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-24 10:18:50 +0100

    Add: Implement 6-print_numberz.c to print numbers from 0 to 9 without using char variable

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - affiche les chiffres en base 10
+ * main - affiche les chiffres en base 10 sans utiliser la variable char
  *
  * Return:0 Si rien ne s'affiche tout est parfait
  */
 int main(void)
 {
-	char nb;
+	int nb;
 
 	for (nb = '0'; nb <= '9'; nb++)
 	{
 		putchar(nb);
 	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
