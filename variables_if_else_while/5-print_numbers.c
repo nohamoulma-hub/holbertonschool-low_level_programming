cde07087c16ad7e1423232a7be65acc6e665e8f2
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-24 10:05:21 +0100

    Add: Implement 5-print_numbers.c to print numbers from 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - affiche l'alphabet sauf la lettre e et q
+ *
+ * Return:0 Si rien ne s'affiche tout est parfait
+ */
+int main(void)
+{
+	char nb;
+
+	for (nb = '0'; nb <= '9'; nb++)
+	{
+		putchar(nb);
+	}
+		putchar('\n');
+		return (0);
+}
