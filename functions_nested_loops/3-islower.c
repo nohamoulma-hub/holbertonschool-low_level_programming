47c1ec78bfb3370074b26709d2230a144f4fef10
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-24 15:39:06 +0100

    fix: change _putchar to return

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,18 +5,17 @@
  * 'c' - c'est l'outil de mesure,il va controler chaque
  * caractère pour savoir si il est minuscule ou non
  *
- * Return:0 Si rien ne s'affiche tout est parfait
+ * Return:1 if lowercase, 0 otherwise
  */
 int _islower(int c)
 
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		_putchar('1');
+		return(1);
 	}
 	else
 	{
-		_putchar('0');
+		return(0);
 	}
-	return (0);
 }
