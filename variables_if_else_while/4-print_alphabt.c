c4795d5961840a26e0ad39471324f860c7059521
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-24 09:54:50 +0100

    Add: Implement 4-print_alphabt.c to print the alphabet excluding letters e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - affiche l'alphabet sauf la lettre e et q
+ *
+ * Return:0 Si rien ne s'affiche tout est parfait
+ */
+int main(void)
+{
+	char lettre;
+
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+	{
+		if (lettre != 'e' && lettre != 'q')
+		{
+			putchar(lettre);
+		}
+	}
+	return (0);
+}
