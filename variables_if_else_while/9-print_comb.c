721bde9dec49de91d9e99469cf48ad8a6cf9c405
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-24 10:53:46 +0100

    Add: Implement 9-print_comb.c to display digits from 0 to 9 separated by commas and spaces

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - affiche la base 10 avec une virgule suivi
+ * d'un espace entre chaque chiffre
+ *
+ * Return:0 Si rien ne s'affiche tout est parfait
+ */
+int main(void)
+{
+	int nb;
+
+	for (nb = '0'; nb <= '9'; nb++)
+	{
+		putchar(nb);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
