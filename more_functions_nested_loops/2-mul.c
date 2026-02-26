32924d1437f038b3c1f9dadcc766b46eb57e8d7d
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-26 12:56:48 +0100

    Add : Write a function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiplie deux entiers.
+ * @a: Le premier entier.
+ * @b: Le deuxième entier.
+ *
+ * Return: Le résultat de la multiplication (a * b).
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
