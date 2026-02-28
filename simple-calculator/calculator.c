7a1fc7a3a21b27ecbd57aa3a093b3d825e72f81f
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-28 12:00:55 +0100

    Add : Implement addition end-to-end.

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(void)
 
 {
-	int choicecalcul;
+	int choicecalcul, nb1 = 0, nb2 = 0, resultat = 0;
 
 		printf("Welcome to simple calculator !\n");
 		printf("1) Add\n");
@@ -21,6 +23,15 @@ int main(void)
 	if (choicecalcul == 0)
 	{
 		printf("Bye !\n");
+	}
+	if (choicecalcul == 1)
+	{
+		printf("Entrez votre premier nombre : \n");
+		scanf("%d", &nb1);
+		printf("Entrez votre deuxième nombre : \n");
+		scanf("%d", &nb2);
+		resultat = nb1 + nb2;
+		printf("%d + %d = %d\n", nb1, nb2, resultat);
 	}
 	return (0);
 }
