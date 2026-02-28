5f4ad5f15038752dae8864ac7470bfdb2b0c7066
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-28 10:48:12 +0100

    Add: Print a welcome line. Display a menu. Allow quitting with option 0.

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+
+{
+	int choicecalcul;
+
+	printf("Welcome to simple calculator !\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	scanf("%d", &choicecalcul);
+	printf("Bye !\n");
+	return (0);
+}
