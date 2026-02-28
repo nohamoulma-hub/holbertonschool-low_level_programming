3ec8a31911a4b7ab1df49167f5a776c6ae60c677
nohamoulma-hub <noham.oulma@gmail.com>
2026-02-28 11:14:28 +0100

    Add :  The menu must loop. If the user enters a number not in the menu, print:

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -4,15 +4,23 @@
 int main(void)
 
 {
-	int choicecalcul;
+	int choicecalcul, add, subtract, mulitiply, divide;
 
-	printf("Welcome to simple calculator !\n");
-	printf("1) Add\n");
-	printf("2) Subtract\n");
-	printf("3) Multiply\n");
-	printf("4) Divide\n");
-	printf("0) Quit\n");
-	scanf("%d", &choicecalcul);
-	printf("Bye !\n");
+		printf("Welcome to simple calculator !\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		scanf("%d", &choicecalcul);
+
+	if (choicecalcul < 0 || choicecalcul > 4)
+	{
+		printf("Invalid choice\n");
+	}
+	if (choicecalcul == 0)
+	{
+		printf("Bye !\n");
+	}
 	return (0);
 }
