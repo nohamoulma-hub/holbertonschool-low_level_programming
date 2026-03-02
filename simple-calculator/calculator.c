267d953fee2fd0cd185798df21a84e3016a3bd0c
nohamoulma-hub <noham.oulma@gmail.com>
2026-03-02 18:55:50 +0100

    fix : Implement multiplication

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -4,43 +4,40 @@
 #include <ctype.h>
 
 int main(void)
-
 {
 	int choicecalcul, nb1 = 0, nb2 = 0, resultat = 0;
 
-		printf("Welcome to simple calculator !\n");
-		printf("1) Add\n");
-		printf("2) Subtract\n");
-		printf("3) Multiply\n");
-		printf("4) Divide\n");
-		printf("0) Quit\n");
+		printf("Welcome to simple calculator !\n1) Add\n2) Subtract\n");
+		printf("3) Multiply\n4) Divide\n0) Quit\n");
 		scanf("%d", &choicecalcul);
-
 	if (choicecalcul < 0 || choicecalcul > 4)
-		{
 		printf("Invalid choice\n");
-		}
-	if (choicecalcul == 0)
-		{
+	else if (choicecalcul == 0)
 		printf("Bye !\n");
-		}
-	if (choicecalcul == 1)
+	else if (choicecalcul == 1)
 	{
-		printf("Entrez votre premier nombre : \n");
-		scanf("%d", &nb1);
-		printf("Entrez votre deuxième nombre : \n");
-		scanf("%d", &nb2);
-		resultat = nb1 + nb2;
-		printf("%d + %d = %d\n", nb1, nb2, resultat);
+		printf("Entrez votre addition : \n");
+		scanf("%d + %d", &nb1, &nb2);
+		printf("%d + %d = %d\n", nb1, nb2, resultat = nb1 + nb2);
 	}
-	if (choicecalcul == 2)
+	else if (choicecalcul == 2)
 	{
-		printf("Entrez votre premier nombre : \n");
-		scanf("%d", &nb1);
-		printf("Entrez votre deuxième nombre : \n");
-		scanf("%d", &nb2);
-		resultat = nb1 - nb2;
-		printf("%d - %d = %d\n", nb1, nb2, resultat);
+		printf("Entrez votre soustraction : \n");
+		scanf("%d - %d", &nb1, &nb2);
+		printf("%d - %d = %d\n", nb1, nb2, resultat = nb1 - nb2);
+	}
+	else if (choicecalcul == 3)
+	{
+		printf("Entrez votre multiplication : \n");
+		scanf("%d * %d", &nb1, &nb2);
+		printf("%d * %d = %d\n", nb1, nb2, resultat = nb1 * nb2);
+	}
+	else if (choicecalcul == 4)
+	{
+		printf("Entrez votre division : \n");
+		scanf("%d / %d", &nb1, &nb2);
+		if (nb2 != 0)
+		printf("%d / %d = %d\n", nb1, nb2, resultat = nb1 / nb2);
 	}
 	return (0);
 }
