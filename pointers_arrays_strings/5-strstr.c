06b69ad21f2a46631142fcdb14d6109ce9806a79
nohamoulma-hub <noham.oulma@gmail.com>
2026-03-05 18:13:44 +0100

    fix : resolutions problems

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,16 +11,21 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (haystack[i] != '\0')
 		{
+			j = 0;
 			while (needle[j] != '\0' && haystack[i + j] == needle[j])
 			{
 				j++;
-				if (needle[j] != '\0')
+				if (needle[j] == '\0')
 				{
 					return (&haystack[i]);
+				}
+				if (*needle == '\0')
+				{
+					return (haystack);
 				}
 			}
 			i++;
