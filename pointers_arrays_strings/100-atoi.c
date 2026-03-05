6998f7ff1eef3ed5932f8849840dcb284599bb77
nohamoulma-hub <noham.oulma@gmail.com>
2026-03-05 08:51:12 +0100

    fix : resolve problems

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -24,7 +24,7 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		resultat = resultat * 10 + (s[i] - '\0');
+		resultat = resultat * 10 + (s[i] - '0');
 		i++;
 	}
 	return (resultat * signe);
