ca6e65f7b8dd7b6114aa3064d1159fcb4d60fa91
nohamoulma-hub <noham.oulma@gmail.com>
2026-03-05 15:25:01 +0100

    fix

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,8 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (accept[n] != '\0' && s[i] != accept[n])
+
+		while (accept[n] != '\0' && s[i] != accept[n])
 			n++;
 	}
 	if (accept[n] == '\0')
