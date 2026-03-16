0123d29f3bf6ea6480a27ce7cb8791761734cbfb
nohamoulma-hub <noham.oulma@gmail.com>
2026-03-16 12:15:05 +0100

    fix : problem with compatibilty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	va_start(args, n);
 	for (i = 0; i < n; i++)
-		total += va_arg(args, unsigned int);
+		total += va_arg(args, int);
 	va_end(args);
 	return (total);
 }
