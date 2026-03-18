e4ceb02ff8b2f3166e8f2f4d24b3019f92bd5070
nohamoulma-hub <noham.oulma@gmail.com>
2026-03-18 15:10:34 +0100

    Add : Write a function that prints anything.

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_all - Prints anything, followed by a new line.
+ * @format: A list of types of arguments passed to the function.
+ * @...: A variable number of arguments to be printed.
+ *
+ * Description: Any argument not of type char, int, float, or
+ * char * is ignored. If a string is NULL, (nil) is printed.
+ */
+void print_all(const char * const format, ...)
+{
+	int index = 0;
+	char *sep = "";
+	char *str;
+	va_list args;
+
+	if (format == NULL)
+		return;
+	va_start(args, format);
+	while (format[index] != '\0')
+	{
+		switch (format[index])
+		{
+		case 'c':
+			printf("%s%c", sep, va_arg(args, int));
+			sep = ", ";
+			break;
+		case 'i':
+			printf("%s%d", sep, va_arg(args, int));
+			sep = ", ";
+			break;
+		case 'f':
+			printf("%s%f", sep, va_arg(args, double));
+			sep = ", ";
+			break;
+		case 's':
+		{
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", sep, str);
+			sep = ", ";
+			break;
+		}
+		default:
+			break;
+		}
+		index++;
+	}
+	va_end(args);
+	printf("\n");
+}
