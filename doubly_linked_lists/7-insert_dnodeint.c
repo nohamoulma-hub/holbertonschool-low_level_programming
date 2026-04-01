7d638794c96f437d03bb2ac7fc87c03c691c615d
nohamoulma-hub <noham.oulma@gmail.com>
2026-04-01 10:06:57 +0200

    Fix insert_dnodeint_at_index to correctly link the new node in the doubly linked list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -34,8 +34,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = cp_h->prev;
 	new_node->next = cp_h;
-	if (cp_h->prev != NULL)
-		cp_h->prev->next = new_node;
-	cp_h->prev = new_node;
+	if (cp_h->next != NULL)
+		cp_h->next->prev = new_node;
+	cp_h->next = new_node;
 	return (new_node);
 }
