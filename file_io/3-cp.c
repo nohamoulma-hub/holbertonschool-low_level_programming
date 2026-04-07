13b7e2f5ed6d77bfa96b0a5433da30deb1f23286
nohamoulma-hub <noham.oulma@gmail.com>
2026-04-07 20:04:06 +0200

    Refactor copy file functionality by extracting content copying logic into a separate function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,6 +5,33 @@
 #include <string.h>
 #include <stdio.h>
 /**
+ * copy_content - Copie le contenu d'un descripteur de fichier vers un autre.
+ * @file_from: Le descripteur du fichier source.
+ * @file_to: Le descripteur du fichier destination.
+ * @av: Le tableau des arguments (pour les messages d'erreur).
+ *
+ * Return: void.
+ */
+void copy_content(int file_from, int file_to, char **av)
+{
+	char buffer[1024];
+	ssize_t byteRead;
+	ssize_t byteWrite;
+
+	while ((byteRead = read(file_from, buffer, 1024)) > 0)
+	{
+		byteWrite = write(file_to, buffer, byteRead);
+		if (byteWrite == -1)
+		{
+			close(file_from);
+			close(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+}
+
+/**
  * main - copies content of a file to another
  * @ac: argument count
  * @av: argument vector
@@ -14,12 +41,10 @@
 int main(int ac, char **av)
 {
 	int file_from, file_to;
-	char buffer[1024];
-	ssize_t byteRead, byteWrite;
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(av[1], O_RDONLY);
@@ -34,16 +59,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	while ((byteRead = read(file_from, buffer, 1024)) > 0)
-	{
-		byteWrite = write(file_to, buffer, byteRead);
-		if (byteWrite == -1)
-		{
-			close(file_from);
-			close(file_to);
-			exit(99);
-		}
-	}
+	copy_content(file_from, file_to, av);
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
